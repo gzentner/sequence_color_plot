2e791efe45b8d0fef4830d02bc6318c1f94022da
Gabe Zentner <gzentner@Gabe-Zentner-MY216B-MacBook-Air.local>
2020-09-27 17:21:44 -0400

    adding script comments

--- sequence_color_plot.R
+++ sequence_color_plot.R
@@ -4,15 +4,19 @@ library(TxDb.Scerevisiae.UCSC.sacCer3.sgdGene)
 library(GenomicRanges)
 library(BSgenome.Scerevisiae.UCSC.sacCer3)
 
-## Load sequences for testing
-promoters <- trim(promoters(TxDb.Scerevisiae.UCSC.sacCer3.sgdGene,
-                            upstream = 50, downstream = 25)) %>%
-  subset(., seqnames %in% c("chrI", "chrII", "chrIII"))
+txdb <- TxDb.Scerevisiae.UCSC.sacCer3.sgdGene
+genome <- BSgenome.Scerevisiae.UCSC.sacCer3
 
+## Load ranges of interest (yeast chrII promoters as example)
+promoters <- trim(promoters(txdb, upstream = 50, downstream = 25)) %>%
+  subset(., seqnames == "chrII")
+
+## Get sequences
 seqs <- getSeq(genome, promoters)
 
 seq_length <- unique(str_length(seqs))
 
+# Generate long table of bases for plotting
 seqs_split <- seqs %>%
   as.character %>%
   str_split(pattern = "", simplify = T) %>%
@@ -21,6 +25,7 @@ seqs_split <- seqs %>%
   rowid_to_column(var = "sequence") %>%
   pivot_longer(-sequence, names_to = "position", values_to = "base")
 
+# Assign a color to each base
 base_cols = c(
   "A" = "#109649",
   "C" = "#255C99",
@@ -28,6 +33,7 @@ base_cols = c(
   "T" = "#D62839"
 )
 
+# Generate color plot
 ggplot(seqs_split, aes(x = position, y = sequence)) +
   geom_tile(aes(fill = base)) +
   theme_minimal() +
